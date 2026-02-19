6a3e466ff74db3fb7b3484c5cfd51c43c0a204f5
depannn <depstoreku@gmail.com>
2026-02-19 18:38:01 +0700

    Upload: constants.tsx

---
+++ constants.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const APP_NAME = "Aplikasi Premium by XTE";
+export const DOMAIN = "app.xte.web.id";
+export const PAYMENT_BASE_URL = "https://blackhat.web.id";
+export const PAYMENT_API_KEY = "51f9cc59799beaa9afc37defb56889e113c706f85e8b0d5c66e8eba5f05c996d";
+export const SUPPORT_WA = "6288242449961";
+export const SUPPORT_EMAIL = "depstoreku@gmail.com";
+
+export const SUPABASE_URL = "https://tojnwagjixprzjcmvkgg.supabase.co";
+export const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvam53YWdqaXhwcnpqY212a2dnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzEyODgwNzksImV4cCI6MjA4Njg2NDA3OX0.TZkcyoeWAF819mVYcz1jZD_YltIVxneBFaR9qvacRSE";
+
+export const COLORS = {
+  primary: "bg-zinc-900",
+  primaryHover: "hover:bg-zinc-800",
+  secondary: "bg-slate-100",
+  border: "border-slate-200",
+};
+
+export const NAVIGATION = [
+  { name: 'Home', path: '/', icon: <i className="fas fa-home"></i> },
+  { name: 'Tutorial', path: '/tutorial', icon: <i className="fas fa-book"></i> },
+  { name: 'FAQ', path: '/faq', icon: <i className="fas fa-question-circle"></i> },
+  { name: 'Terms', path: '/terms', icon: <i className="fas fa-file-contract"></i> },
+];
